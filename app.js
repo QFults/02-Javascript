f5cd5e6a99a8988e87225190b95687a32b7ec74c
Quinton Fults <quinton.fults.work@gmail.com>
2020-10-27 10:56:40 -0700

    code

---
+++ app.js
@@ -0,0 +1,20 @@
+// Declaring arrays
+
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// -------------------
+
+// Indexing arrays
+
+// Counting starts from 0
+// console.log(nums[3])
+
+// -------------------
+
+// Arrays can have mixed types
+let mess = ['hello', 5, true, true, 12, 'something', ['a', 'b', 'c'], false]
+
+// Grabbing from an array within an array
+console.log(mess[6][2])
+
+// -------------------
