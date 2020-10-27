c2b45e0f14958326f7259453d1f2ce3cc55ca4c6
Quinton Fults <quinton.fults.work@gmail.com>
2020-10-27 11:34:24 -0700

    code

--- app.js
+++ app.js
@@ -12,9 +12,23 @@
 // -------------------
 
 // Arrays can have mixed types
-let mess = ['hello', 5, true, true, 12, 'something', ['a', 'b', 'c'], false]
+// let mess = ['hello', 5, true, true, 12, 'something', ['a', 'b', 'c'], false]
 
 // Grabbing from an array within an array
-console.log(mess[6][2])
+// console.log(mess[6][2])
 
 // -------------------
+
+// let movies = ['Your Name', 'Airplane!', 'Naked Gun', 'Apocalyse Now', 'Scary Movie 3']
+
+// let songs = ['Concerto No. 1 in G Major', 'Symphony No. 5', 'Invention No. 4', 'Thriller', 'Alexander Hamilton']
+
+// let foods = ['sushi', 'snickers', 'water', 'bird food', 'sweet potato']
+
+// let games = ['Yakuza 0', 'Divinity: Original Sin 2', 'Fall Guys', 'The Master Chief Collection', 'Stardew Valley']
+
+let alpha = ['a', 'b', 'c']
+
+for (let i = 0; i < alpha.length; i++) {
+  console.log(alpha[i])
+}
